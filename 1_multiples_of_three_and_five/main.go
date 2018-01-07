baa1e6a3a232f384e4ed1b5b162768968bd089d3
Michael Roberts <MikeyRobby@users.noreply.github.com>
2018-01-07 00:28:11 -0800

    Added a comment for a better way to achieve goal.

--- 1_multiples_of_three_and_five/main.go
+++ 1_multiples_of_three_and_five/main.go
@@ -19,8 +19,19 @@ func math(number int)int {
 	}
 	total := mulfive + multhree
 	return total
-}
+	
+	/* Another way to do it
 
+func Multiple3And5(number int) int {
+  var count int
+  for i := 0; i < number; i++{
+    if i % 5 == 0 || i % 3 == 0{
+      count += i
+      }
+    }
+  return count
+}
+*/
 func main() {
 	fmt.Print(math(1000))
 
